19d62d6bf527000ddcf89e43ffeea812a0348208
Mahin05 <mahin.islam05@gmail.com>
2022-07-28 17:19:20 +0600

    cancel button functionalities implemented

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -39,6 +39,14 @@ const Form = () => {
         setRemark('');
     }
 
+    // Cancel button functionalities
+    const handleCancelButton = (e) => {
+        setDate('');
+        setAmount('');
+        setPaymentMethod('');
+        setRemark('');
+    }
+
     // saving data to local storage
     useEffect(() => {
         localStorage.setItem('receipts', JSON.stringify(receipts));
@@ -67,7 +75,7 @@ const Form = () => {
                     </div>
                     <div className='button-style'>
                         <div>
-                            <button>CANCEL</button>
+                            <button onClick={handleCancelButton}>CANCEL</button>
                         </div>
                         <div>
                             <button>SUBMIT</button>
@@ -94,35 +102,6 @@ const Form = () => {
                     </div>
                 </>}
                 {receipts.length < 1 && <div>No records available</div>}
-                {/* <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td colspan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </table> */}
             </div>
         </div>
     );
